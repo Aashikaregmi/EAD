6d9266cc4e2c93bcee412492536831be989fb7f4
Aashikaregmi <aashikaregmi1234@gmail.com>
2025-05-19 23:14:12 +0545

    lab2

---
+++ Lab2/DesignPatterns/singleton/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Only one instance of ShapeFactory
+        ShapeMaker factory = ShapeMaker.getInstance();
+
+        Shape circle = factory.getShape("circle");
+        circle.draw();  // Output: Drawing a Circle
+
+        Shape rectangle = factory.getShape("rectangle");
+        rectangle.draw();  // Output: Drawing a Rectangle
+    }
+}
